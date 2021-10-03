579975e8c8e40fe2c67563708bae502d66520e28
Khang Le <tkhang1999@gmail.com>
2021-10-03 23:37:44 +0800

    upgrade dependencies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
                 <a href="https://www.linkedin.com/in/tan-khang-le/" className="footer__icon"><i className='bx bxl-linkedin' ></i></a>
                 <a href="https://github.com/tkhang1999" className="footer__icon"><i className='bx bxl-github' ></i></a>
             </div>
-            <p>&#169; 2021 copyright all right reserved</p>
+            <p>&#169; 2021 Khang Le. All Rights Reserved.</p>
         </footer>
     );
 }
