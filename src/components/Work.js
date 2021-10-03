6ea8445c74abb6d76d6bc0a99f4142d21014ec4f
Khang Le <tkhang1999@gmail.com>
2021-10-03 12:03:45 +0800

    complete work experience

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,7 +7,7 @@ const Work = () => {
                     <div>
                         <h3><a className="work__subtitle" href="https://www.goldmansachs.com/">Goldman Sachs</a></h3>
                         <p className="work__text">
-                            Software Engineer <br/>
+                            Analyst (Software Engineer) <br/>
                             <i>Jul 2021 - Present</i>
                         </p>
                     </div>
@@ -46,6 +46,36 @@ const Work = () => {
                         <li>Developed machine learning models that detect and reconcile regulatory breaches based on partial account data</li>
                         <li>Researched and proposed the open-source Conflict-free Replicated Data Type (CRDT) Counter designs that
                             support distributed systems and focus on eventual consistency</li>
+                    </dl>
+                </div>
+
+                <div className="work__container bd-grid">
+                    <div>
+                        <h3><a className="work__subtitle" href="https://noviscient.com/">Noviscient</a></h3>
+                        <p className="work__text">
+                            Junior Engineer (Apprenticeship under <a href="https://www.sginnovate.com/talentprogrammes">SGInnovate Summation Programme</a>) <br/>
+                            <i>May 2019 - Aug 2019</i>
+                        </p>
+                    </div>
+                    <dl>
+                        <li>Built a web application that gives investors and alpha partners access to portfolio creation and monitoring tools</li>
+                        <li>Used different technologies such as Django, Docker, PostgreSQL, and jQuery for web full-stack development</li>
+                        <li>Liaised with CTO to deploy the demo website successfully for investors</li>
+                    </dl>
+                </div>
+
+                <div className="work__container bd-grid">
+                    <div>
+                        <h3><a className="work__subtitle" href="https://www.ackcio.com/">Ackcio</a></h3>
+                        <p className="work__text">
+                            Intern Software Engineer <br/>
+                            <i>May 2018 - Aug 2018</i>
+                        </p>
+                    </div>
+                    <dl>
+                        <li>Developed a software running on Raspberry Pi to read and transmit real-time data for construction monitoring</li>
+                        <li>Utilized the multi-threading technique for concurrent execution in communicating serial data using Python</li>
+                        <li>Worked with senior engineers to deploy the software successfully to construction sites in Singapore and Korea</li>
                     </dl>
                 </div>
             </div>
