7813b68af9936b77afe55c84459af51d48bbbe6e
Khang Le <tkhang1999@gmail.com>
2021-10-03 14:48:12 +0800

    fix home display

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,15 @@ import profileImg from "../images/profile-pic.png";
 const Home = () => {
     return (
         <section className="home bd-grid" id="home">
-            <div className="home__data">
-                <h1 className="home__title">Hi,<br />I'm <span className="home__title-color">Khang</span><br />Solid Software Engineer</h1>
-
-                <a href="mailto:tkhang1999@gmail.com" className="button">Contact</a>
-            </div>
-
-            <div className="home__social">
-                <a href="https://www.linkedin.com/in/tan-khang-le/" className="home__social-icon"><i className='bx bxl-linkedin'></i></a>
-                <a href="https://github.com/tkhang1999" className="home__social-icon"><i className='bx bxl-github' ></i></a>
+            <div>
+                <div className="home__data">
+                    <h1 className="home__title">Hi,<br />I'm <span className="home__title-color">Khang</span>.<br />Solid Software Engineer</h1>
+                    <a href="mailto:tkhang1999@gmail.com" className="button">Contact</a>
+                </div>
+                <div className="home__social">
+                    <a href="https://www.linkedin.com/in/tan-khang-le/" className="home__social-icon"><i className='bx bxl-linkedin'></i></a>
+                    <a href="https://github.com/tkhang1999" className="home__social-icon"><i className='bx bxl-github' ></i></a>
+                </div>
             </div>
 
             <div className="home__img">
