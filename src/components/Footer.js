ce0657fe1b6db85371bb763fc309df1dd83dd96f
Khang Le <tkhang1999@gmail.com>
2021-10-03 12:11:53 +0800

    fix styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
     return (
-        <footer class="footer">
-            <p class="footer__title">Khang Le</p>
-            <div class="footer__social">
-                <a href="https://www.linkedin.com/in/tan-khang-le/" class="footer__icon"><i class='bx bxl-linkedin' ></i></a>
-                <a href="https://github.com/tkhang1999" class="footer__icon"><i class='bx bxl-github' ></i></a>
+        <footer className="footer">
+            <p className="footer__title">Khang Le</p>
+            <div className="footer__social">
+                <a href="https://www.linkedin.com/in/tan-khang-le/" className="footer__icon"><i className='bx bxl-linkedin' ></i></a>
+                <a href="https://github.com/tkhang1999" className="footer__icon"><i className='bx bxl-github' ></i></a>
             </div>
             <p>&#169; 2021 copyright all right reserved</p>
         </footer>
