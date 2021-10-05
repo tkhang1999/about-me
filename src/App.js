804914a9ea09da424adb307829666a4013454b6d
Khang Le <tkhang1999@gmail.com>
2021-10-06 01:25:20 +0800

    change from tab to space indent

--- src/App.js
+++ src/App.js
@@ -10,64 +10,64 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 const scrollReveal = () => {
-	const sr = ScrollReveal({
-		origin: 'top',
-		distance: '80px',
-		duration: 600,
-		reset: true
-	});
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 600,
+        reset: true
+    });
 
-	/*SCROLL HOME*/
-	sr.reveal('.home__title',{});
-	sr.reveal('.button',{delay: 200});
-	sr.reveal('.home__img',{delay: 200});
-	sr.reveal('.home__social-icon',{ interval: 200});
+    /*SCROLL HOME*/
+    sr.reveal('.home__title',{});
+    sr.reveal('.button',{delay: 200});
+    sr.reveal('.home__img',{delay: 200});
+    sr.reveal('.home__social-icon',{ interval: 200});
 
-	/*SCROLL ABOUT*/
-	sr.reveal('.about__img',{});
-	sr.reveal('.about__subtitle',{});
-	sr.reveal('.about__text',{});
+    /*SCROLL ABOUT*/
+    sr.reveal('.about__img',{});
+    sr.reveal('.about__subtitle',{});
+    sr.reveal('.about__text',{});
 
-	/*SCROLL SKILLS*/
-	sr.reveal('.skills__subtitle',{});
-	sr.reveal('.skills__text',{});
-	sr.reveal('.skills__list',{});
+    /*SCROLL SKILLS*/
+    sr.reveal('.skills__subtitle',{});
+    sr.reveal('.skills__text',{});
+    sr.reveal('.skills__list',{});
 
-	/*SCROLL PROJECTS*/
-	sr.reveal('.projects__list',{});
+    /*SCROLL PROJECTS*/
+    sr.reveal('.projects__list',{});
 
-	/*SCROLL WORK*/
-	sr.reveal('.work__list',{});
+    /*SCROLL WORK*/
+    sr.reveal('.work__list',{});
 }
 
 const App = () => {
-	useEffect(() => {
-		scrollReveal();
-	}, []);
+    useEffect(() => {
+        scrollReveal();
+    }, []);
 
-	return (
-		<div>
-			<header className="l-header">
-				{/* ===== NAVBAR ===== */}
-				<NavBar />
-			</header>
-			<main className="l-main">
-				{/* ===== HOME ===== */}
-				<Home />
-				{/* ===== ABOUT ===== */}
-				<About />
-				{/* ===== SKILLS ===== */}
-				<Skills />
-				{/* ===== PROJECTS ===== */}
-				<Projects />
-				{/* ===== WORK ===== */}
-				<Work />
-			</main>
+    return (
+        <div>
+            <header className="l-header">
+                {/* ===== NAVBAR ===== */}
+                <NavBar />
+            </header>
+            <main className="l-main">
+                {/* ===== HOME ===== */}
+                <Home />
+                {/* ===== ABOUT ===== */}
+                <About />
+                {/* ===== SKILLS ===== */}
+                <Skills />
+                {/* ===== PROJECTS ===== */}
+                <Projects />
+                {/* ===== WORK ===== */}
+                <Work />
+            </main>
 
-			{/* ===== FOOTER ===== */}
-			<Footer />
-		</div>
-	);
+            {/* ===== FOOTER ===== */}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
