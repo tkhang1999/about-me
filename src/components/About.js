5389a5b2598dba0b52c22f83632e9ba874f1ddb2
Khang Le <tkhang1999@gmail.com>
2023-01-28 16:51:00 +0800

    shorten about

--- src/components/About.js
+++ src/components/About.js
@@ -16,14 +16,8 @@ const About = () => {
             engineer with great focus in developing full-stack applications and
             exploring new technologies. I graduated from Nanyang Technological
             University with a Bachelor of Engineering in Computer Science with
-            Honours (Highest Distinction).
-            <br />
-            <br />
-            My interests are in machine learning and software enginerring. To
-            pursue them, I have taken two elective focuses during college, one
-            in Data Science and Analytics and the other in High Performance
-            Computing. In my free time, I enjoy cooking, jogging, and playing
-            table tennis and badminton.
+            Honours (Highest Distinction). In my free time, I enjoy cooking,
+            jogging, and playing table tennis and badminton.
           </p>
         </div>
       </div>
