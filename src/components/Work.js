0ca3ac7b07ad2f27d151a393ace6609b53d57e15
Khang Le <tkhang1999@gmail.com>
2023-12-08 02:22:38 -0800

    update work details

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,78 +9,143 @@ const Work = () => {
     <section className="work section" id="work">
       <h2 className="section-title">Work Experience</h2>
       <div className="work__list bd-container">
-        <div className="work__container work__grid">
-          <img className="work__img" src={sfuImg} alt="sfu-logo" />
+        <div className="work__container">
+          <div className="work__grid">
+            <img className="work__img" src={sfuImg} alt="sfu-logo" />
+            <p className="work__title">
+              Graduate Research Assistant @{" "}
+              <a className="external__link" href="https://www.sfu.ca/">
+                SFU
+              </a>{" "}
+              <br />
+              <i>Sep 2023 - Present</i>
+            </p>
+          </div>
           <p className="work__text">
-            Graduate Research Assistant @{" "}
-            <a className="external__link" href="https://www.sfu.ca/">
-              SFU
-            </a>{" "}
+            Work on research projects in software engineering and programming
+            languages, specifically symbolic execution and formal verification.
             <br />
-            <i>Sep 2023 - Present</i>
-          </p>
-        </div>
-        <div className="work__container work__grid">
-          <img className="work__img" src={gsImg} alt="goldman-sachs-logo" />
-          <p className="work__text">
-            Software Engineer @{" "}
-            <a className="external__link" href="https://www.goldmansachs.com/">
-              Goldman Sachs
-            </a>{" "}
             <br />
-            <i>Jul 2021 - Aug 2023</i>
-          </p>
-        </div>
-
-        <div className="work__container work__grid">
-          <img className="work__img" src={gsImg} alt="Goldman Sachs" />
-          <p className="work__text">
-            Summer Analyst @{" "}
-            <a className="external__link" href="https://www.goldmansachs.com/">
-              Goldman Sachs
-            </a>{" "}
-            <br />
-            <i>Jul 2020 - Aug 2020</i>
+            <i>Tech stack: Rust, Dafny, Linux</i>
           </p>
         </div>
 
-        <div className="work__container work__grid">
-          <img className="work__img" src={paypalImg} alt="paypal-logo" />
+        <div className="work__container ">
+          <div className="work__grid">
+            <img className="work__img" src={gsImg} alt="goldman-sachs-logo" />
+            <p className="work__title">
+              Software Engineer @{" "}
+              <a
+                className="external__link"
+                href="https://www.goldmansachs.com/"
+              >
+                Goldman Sachs
+              </a>{" "}
+              <br />
+              <i>Jul 2021 - Aug 2023</i>
+            </p>
+          </div>
           <p className="work__text">
-            Software Engineering Intern @{" "}
-            <a className="external__link" href="https://www.paypal.com/">
-              PayPal
-            </a>{" "}
+            Maintained various in-house applications covering external
+            connectivity and post-trade allocations to support the listed
+            derivatives clearing business.
             <br />
-            <i>Aug 2019 - Dec 2019</i>
+            <br />
+            <i>
+              Tech stack: Java, TypeScript, Spring Boot, React, SQL, Kafka,
+              Docker
+            </i>
           </p>
         </div>
 
-        <div className="work__container work__grid">
-          <img
-            className="work__img"
-            src={noviscientImg}
-            alt="noviscient-logo"
-          />
+        <div className="work__container">
+          <div className="work__grid">
+            <img className="work__img" src={gsImg} alt="Goldman Sachs" />
+            <p className="work__title">
+              Summer Analyst @{" "}
+              <a
+                className="external__link"
+                href="https://www.goldmansachs.com/"
+              >
+                Goldman Sachs
+              </a>{" "}
+              <br />
+              <i>Jul 2020 - Aug 2020</i>
+            </p>
+          </div>
           <p className="work__text">
-            Junior Engineer @{" "}
-            <a className="external__link" href="https://noviscient.com/">
-              Noviscient
-            </a>{" "}
+            Built a full-stack web dashboard for memory usage monitoring of
+            multiple applications and concurrent processes running in the firm.
             <br />
-            <i>May 2019 - Aug 2019</i>
+            <br />
+            <i>Tech stack: Java, TypeScript, Spring Boot, React, Kafka</i>
           </p>
         </div>
 
-        <div className="work__container work__grid">
-          <img className="work__img" src={ackcioImg} alt="ackcio-logo" />
+        <div className="work__container">
+          <div className="work__grid">
+            <img className="work__img" src={paypalImg} alt="paypal-logo" />
+            <p className="work__title">
+              Software Engineering Intern @{" "}
+              <a className="external__link" href="https://www.paypal.com/">
+                PayPal
+              </a>{" "}
+              <br />
+              <i>Aug 2019 - Dec 2019</i>
+            </p>
+          </div>
           <p className="work__text">
-            Intern Software Engineer @{" "}
-            <a className="external__link" href="https://www.ackcio.com/">
-              Ackcio
-            </a>{" "}
+            Worked on multiple RESTful web services in the Compliance platform
+            and developed machine learning models to detect regulatory breaches.
             <br />
-            <i>May 2018 - Aug 2018</i>
+            <br />
+            <i>Tech stack: Java, Python, Spring Boot, SQL</i>
+          </p>
+        </div>
+
+        <div className="work__container">
+          <div className="work__grid">
+            <img
+              className="work__img"
+              src={noviscientImg}
+              alt="noviscient-logo"
+            />
+            <p className="work__title">
+              Junior Engineer @{" "}
+              <a className="external__link" href="https://noviscient.com/">
+                Noviscient
+              </a>{" "}
+              <br />
+              <i>May 2019 - Aug 2019</i>
+            </p>
+          </div>
+          <p className="work__text">
+            Built a full-stack web application that gives investors and alpha
+            partners access to portfolio creation and monitoring tools.
+            <br />
+            <br />
+            <i>Tech stack: Python, Django, jQuery, Docker</i>
+          </p>
+        </div>
+
+        <div className="work__container">
+          <div className="work__grid">
+            <img className="work__img" src={ackcioImg} alt="ackcio-logo" />
+            <p className="work__title">
+              Intern Software Engineer @{" "}
+              <a className="external__link" href="https://www.ackcio.com/">
+                Ackcio
+              </a>{" "}
+              <br />
+              <i>May 2018 - Aug 2018</i>
+            </p>
+          </div>
+          <p className="work__text">
+            Developed a multi-threaded software running on
+            Raspberry Pi to concurrently read and transmit serial data for
+            real-time construction monitoring. <br />
+            <br />
+            <i>Tech stack: Python, SQL, Linux</i>
           </p>
         </div>
       </div>
